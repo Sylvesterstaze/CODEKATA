cc3d24d6d03d32d39366cd3fb3a8b2d1ad4c50a4
Sylvesterstaze <99717417+Sylvesterstaze@users.noreply.github.com>
2022-08-20 10:16:16 +0530

    Add files via upload
    Write a code to get 2 integers as input and find the HCF of the 2 integer without using recursion or Euclidean algorithm.
    
    Input Description:
    A single line containing 2 integers separated by space.
    
    Output Description:
    Print the HCF of the integers.
    
    Sample Input :
    2 3
    Sample Output :
    1

---
+++ GUVI__ABSOLUTE__BEGINNER/30__absolute_beginner.py
@@ -0,0 +1,13 @@
+num1,num2=input().split()
+num1=int(num1)
+num2=int(num2)
+def compute_hcf(x, y):
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller+1):
+        if((x % i == 0) and (y % i == 0)):
+            hcf = i 
+    return hcf
+print(compute_hcf(num1, num2))
